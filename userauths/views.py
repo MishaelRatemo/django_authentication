b836cc4189e0f817587257336a5e1cecc8e25212
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-03 08:21:01 +0300

    updates

--- userauths/views.py
+++ userauths/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render, HttpResponseRedirect
 from django.contrib.auth import authenticate, logout as auth_logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def home (request):
@@ -8,4 +9,5 @@ def home (request):
 
 def logout(request):
     auth_logout(request)
+    messages.success(request, 'Logged out successfully')
     return HttpResponseRedirect('/')
