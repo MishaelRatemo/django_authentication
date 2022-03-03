b4d56dda882ca385ee6e5af9d5367da3a05b2f59
MishaelRatemo <mishael.ratemo@student.moringaschool.com>
2022-03-03 07:44:34 +0300

    added basic views

--- userauths/urls.py
+++ userauths/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='index' ),
+    path('logout', views.logout, name='logout'),
+    
 ]
--- userauths/views.py
+++ userauths/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import redirect, render, HttpResponseRedirect
+from django.contrib.auth import authenticate, logout as auth_logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def home (request):
-    return HttpResponse('<h1> Welcome Home Of forests</h1>')
+    return render(request, 'index.html')
+
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect('/')
